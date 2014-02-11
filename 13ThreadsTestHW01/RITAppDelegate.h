8ce1c8d13fed42df07e85faf1daa2dc879c4ad65
Pronin Alexander <pronin.alx@gmail.com>
2014-02-11 20:52:14 +0200

    Initial Commit

---
+++ 13ThreadsTestHW01/RITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RITAppDelegate.h
+//  13ThreadsTestHW01
+//
+//  Created by Aleksandr Pronin on 11.02.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
